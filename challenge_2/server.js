c9e94939770916afb3aa695780aa1bfe27638839
j-ornelas <johnnyoh1988@gmail.com>
2018-02-13 10:57:03 -0800

    Sets up basic workstation:

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,63 @@
+var express = require('express');
+var app = express();
+var path = require('path');
 
+app.use(express.static('client'));
 
+var csv = {
+    "firstName": "Joshie",
+    "lastName": "Wyattson",
+    "county": "San Mateo",
+    "city": "San Mateo",
+    "role": "Broker",
+    "sales": 1000000,
+    "children": [
+    {
+      "firstName": "Beth Jr.",
+      "lastName": "Johnson",
+      "county": "San Mateo",
+      "city": "Pacifica",
+      "role": "Manager",
+      "sales": 2900000,
+      "children": [
+        {
+          "firstName": "Smitty",
+          "lastName": "Won",
+          "county": "San Mateo",
+          "city": "Redwood City",
+          "role": "Sales Person",
+          "sales": 4800000,
+          "children": []
+        },
+        {
+          "firstName": "Allen",
+          "lastName": "Price",
+          "county": "San Mateo",
+          "city": "Burlingame",
+          "role": "Sales Person",
+          "sales": 2500000,
+          "children": []
+        }
+      ]
+    },
+    {
+      "firstName": "Beth",
+      "lastName": "Johnson",
+      "county": "San Francisco",
+      "city": "San Francisco",
+      "role": "Broker/Sales Person",
+      "sales": 7500000,
+      "children": []
+    }
+  ]
+};
+
+app.get('/', function( req, res) {
+  res.sendFile(path.join(__dirname + '/client/'));
+})
+
+app.get('/csv', function( req, res ) {
+  console.log(csv)
+})
+
+app.listen(3000, () => console.log('server running on port 3000'))
