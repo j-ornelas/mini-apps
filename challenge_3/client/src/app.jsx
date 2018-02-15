fa0c2a454b9b1d1b73c404936d20be3533855d3b
j-ornelas <johnnyoh1988@gmail.com>
2018-02-14 16:06:05 -0800

    All rows have gravity logic and update with circle

--- challenge_3/client/src/app.jsx
+++ challenge_3/client/src/app.jsx
@@ -6,8 +6,80 @@ class App extends React.Component {
   constructor(){
     super();
     this.state = {
-      redTurn: true
+      redTurn: true,
+      // 0 is empty, 1 is red, 2 is black
+      board: [[0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0],
+              [0,0,0,0,0,0,0]]
     }
+  }
+
+  addColor(){
+    if (this.state.redTurn === true) {
+      return "red_circle"
+    } else {
+      return "black_circle"
+    }
+  }
+
+  endTurn(){
+    if (this.state.redTurn){
+      this.setState({redTurn: false})
+    } else{
+      this.setState({redTurn: true})
+    }
+  }
+
+  insertIntoColumn(index) {
+    let board = this.state.board;
+    let placed = false;
+    let color = this.addColor()
+
+    for (var i = 0; i < board.length; i++) {
+      if (board[0][index] !== 0){
+        alert('this column is filled')
+        return;
+      }
+      if (board[i][index] === 0){
+        continue;
+      } else if (board[i][index] !== 0) {
+        if (this.state.redTurn === true){
+          board[i-1][index] = 1;
+          placed = true;
+          $('.' + (i-1) + '-' + index).addClass("red_circle")
+          this.endTurn()
+          return;
+        } else if (this.state.redTurn === false){
+          board[i-1][index] = 2;
+          placed = true;
+          $('.' + (i-1) + '-' + index).addClass("black_circle")
+          this.endTurn()
+          return;
+        }
+      }
+    }
+    if (!placed){
+      // board[5][index] = 1 or 2
+      if (this.state.redTurn){
+        board[5][index] = 1
+        $('.' + 5 + '-' + index).addClass("red_circle")
+        this.endTurn()
+      } else {
+        board[5][index] = 2
+        $('.' + 5 + '-' + index).addClass("black_circle")
+        this.endTurn()
+      }
+    }
+    // this.setState({board: board})
+  }
+
+  handleClick(column) {
+    //insert into column here
+    this.insertIntoColumn(column)
+    console.log(this.state.redTurn)
   }
 
   render() {
@@ -15,7 +87,7 @@ class App extends React.Component {
 
       <div>
         <h1>This is the App Component</h1>
-        <Board />
+        <Board handleClick={this.handleClick.bind(this)}/>
       </div>
 
     );
--- challenge_3/client/src/modules/board.jsx
+++ challenge_3/client/src/modules/board.jsx
@@ -4,26 +4,36 @@ import ReactDOM  from 'react-dom';
 export default function Board (props) {
 
     return (
-    <div><p>the board!</p>
+    <div>
+      <div>
+        <button className="col1" onClick={props.handleClick.bind(this, 0)}>click</button>
+        <button className="col2" onClick={props.handleClick.bind(this, 1)}>click</button>
+        <button className="col3" onClick={props.handleClick.bind(this, 2)}>click</button>
+        <button className="col4" onClick={props.handleClick.bind(this, 3)}>click</button>
+        <button className="col5" onClick={props.handleClick.bind(this, 4)}>click</button>
+        <button className="col6" onClick={props.handleClick.bind(this, 5)}>click</button>
+        <button className="col7" onClick={props.handleClick.bind(this, 6)}>click</button>
+
+      </div>
       <table>
        <tbody>
-        <tr>
-          <td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+        <tr className="row_one">
+          <td className="0-0"></td><td className="0-1"></td><td className="0-2"></td><td className="0-3"></td><td className="0-4"></td><td className="0-5"></td><td className="0-6"></td>
         </tr>
-        <tr>
-          <td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+        <tr className="row_two">
+          <td className="1-0"></td><td className="1-1"></td><td className="1-2"></td><td className="1-3"></td><td className="1-4"></td><td className="1-5"></td><td className="1-6"></td>
         </tr>
-        <tr>
-          <td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+        <tr className="row_three">
+          <td className="2-0"></td><td className="2-1"></td><td className="2-2"></td><td className="2-3"></td><td className="2-4"></td><td className="2-5"></td><td className="2-6"></td>
         </tr>
-        <tr>
-          <td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+        <tr className="row_four">
+          <td className="3-0"></td><td className="3-1"></td><td className="3-2"></td><td className="3-3"></td><td className="3-4"></td><td className="3-5"></td><td className="3-6"></td>
         </tr>
-        <tr>
-          <td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+        <tr className="row_five">
+          <td className="4-0"></td><td className="4-1"></td><td className="4-2"></td><td className="4-3"></td><td className="4-4"></td><td className="4-5"></td><td className="4-6"></td>
         </tr>
-        <tr>
-          <td></td><td></td><td></td><td></td><td></td><td></td><td></td><td></td>
+        <tr className="row_six">
+          <td className="5-0"></td><td className="5-1"></td><td className="5-2"></td><td className="5-3"></td><td className="5-4"></td><td className="5-5"></td><td className="5-6"></td>
         </tr>
        </tbody>
       </table>
